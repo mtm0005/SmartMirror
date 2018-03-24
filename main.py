3a7d20fa426620698c7aef43d360284bfa46eff9
Tyler Sherrod <tylersherrod@Tylers-MacBook-Pro.local>
2018-03-24 15:19:00 -0500

    Clean-up code and removed padding around border

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import tkinter as tk
-import pdb
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master):
         super().__init__(master)
         self.pack()
         self.create_widgets()
-        self.master=master
-        pad=3
-        self._geom='200x200+0+0'
+        self.master = master
+        self._geom = '200x200+0+0'
         master.geometry("{0}x{1}+0+0".format(
-            master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
-        master.bind('<Escape>',self.toggle_geom)
+            master.winfo_screenwidth(), master.winfo_screenheight()))
+        master.bind('<Escape>', self.toggle_geom)
         master.overrideredirect(1)
 
     def create_widgets(self):
@@ -21,18 +19,18 @@ class Application(tk.Frame):
         self.hi_there.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
+                              command=self.master.destroy)
         self.quit.pack(side="bottom")
 
     def say_hi(self):
         print("hi there, everyone!")
 
-    def toggle_geom(self,event):
-        geom=self.master.winfo_geometry()
-        print(geom,self._geom)
+    def toggle_geom(self, event):
+        geom = self.master.winfo_geometry()
+        print(geom, self._geom)
         self.master.geometry(self._geom)
-        self._geom=geom
+        self._geom = geom
 
 root = tk.Tk()
-app = Application(master=root)
+app = Application(root)
 app.mainloop()
